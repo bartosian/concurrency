83a21503786ad536a1225a9c5cb1ccbc4426df29
bartosian <bartosian1989@gmail.com>
2020-04-08 08:25:28 -0400

    Added shell implemenattion

---
+++ shell.C
@@ -0,0 +1,16 @@
+main() {
+    char *prog = NULL;
+    char **args = NULL;
+
+    while (readAndParseCmdLIne(&prog, &args)) {
+        int child_pid = fork();
+
+        if (child_pid == 0) {
+            exec(prog, args);
+        
+         } esle {
+             wait(child_pid);
+             return 0;
+         }
+    }
+}
